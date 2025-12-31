3e13de9564e8664fc13ce2ba68a1fc2e9d369fce
max liu <max.liu@digitimes.com>
2025-12-31 16:26:20 +0800

    add list component to main; update main template and imports

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from "../shared/components/header/header";
+import { List } from "../shared/components/list/list";
 
 @Component({
 	selector: 'app-main',
 	imports: [
-		Header
+		Header,
+		List
 	],
 	templateUrl: './main.html',
 	styleUrl: './main.css',
