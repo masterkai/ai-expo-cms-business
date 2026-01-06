ebe65b5719ed836f1ee84bf1a9ca48515584b2f3
max liu <max.liu@digitimes.com>
2026-01-06 12:02:29 +0800

    新增: 參展廠商頁面新增按鈕以開啟對話框和匯出CSV功能

--- src/app/tables/exhibitors/exhibitors.ts
+++ src/app/tables/exhibitors/exhibitors.ts
@@ -2,17 +2,24 @@ import { Component, inject, signal } from '@angular/core';
 import { TableModule } from "primeng/table";
 import { ExhibitorItem, ExhibitorService } from "../../services/exhibitor.service";
 import { DatePipe } from "@angular/common";
+import { AppDialogService } from "../../services/app-dialog.service";
+import {
+	ExhibitionRightsSettingProcess
+} from "../../exhibition-rights-setting-process/exhibition-rights-setting-process";
+import { Button } from "primeng/button";
 
 @Component({
 	selector: 'app-exhibitors',
 	imports: [
 		TableModule,
-		DatePipe
+		DatePipe,
+		Button
 	],
 	templateUrl: './exhibitors.html',
 	styleUrl: './exhibitors.css',
 })
 export class Exhibitors {
+	dialog = inject(AppDialogService)
 	exhibitorsService = inject(ExhibitorService)
 	exhibitors = signal<ExhibitorItem[]>([])
 
@@ -20,5 +27,24 @@ export class Exhibitors {
 		this.exhibitorsService.getExhibitors().then(data => {
 			this.exhibitors.set(data)
 		})
+	}
+
+	openDialog() {
+		const ref = this.dialog.open(ExhibitionRightsSettingProcess, {
+			header: '建立專屬連結',
+			width: '90%',
+			data: {
+				userId: 1,
+				name: 'Max'
+			}
+		});
+
+		ref.onClose.subscribe(result => {
+			console.log('Dialog 回傳：', result);
+		});
+	}
+
+	protected exportExcel() {
+		console.log('exportExcel');
 	}
 }
