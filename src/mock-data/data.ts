cde168dbdd4bba5232c839f2f9af224026018ecf
max liu <max.liu@digitimes.com>
2026-01-06 12:25:11 +0800

    新增: 新增多個下拉選單資料以支援講座議程、展位、媒體推廣等功能

--- src/mock-data/data.ts
+++ src/mock-data/data.ts
@@ -1,4 +1,4 @@
-const Unsure_of_the_lecture_agenda_drop_down_menu = [
+export const Unsure_of_the_lecture_agenda_drop_down_menu: Select_item[] = [
 	{
 		value: '3/25(三) 11:00-11:20   鑽石贊助'
 	},
@@ -45,3 +45,119 @@ const Unsure_of_the_lecture_agenda_drop_down_menu = [
 		value: '3/25(三) 15:30-15:40   一般贊助'
 	},
 ]
+
+export const Future_Stage_Agenda_Drop_down_Menu: Select_item[] = [
+	{
+		value: '33/25(三) 10:30-10:50'
+	},
+	{
+		value: '3/25(三) 10:50-11:10'
+	},
+	{
+		value: '3/25(三) 11:10-11:30'
+	},
+	{
+		value: '3/26(四) 11:00-11:20'
+	},
+	{
+		value: '3/26(四) 11:20-11:40'
+	},
+	{
+		value: '3/26(四) 14:00-14:20'
+	},
+	{
+		value: '3/26(四) 14:20-14:40'
+	},
+	{
+		value: '3/27(五) 12:00-16:30'
+	},
+]
+
+export const Exhibition_booth_drop_down_menu: Select_item[] = [
+	{
+		value: 'A區-01'
+	},
+	{
+		value: 'A區-02'
+	},
+	{
+		value: 'A區-03'
+	},
+	{
+		value: 'A區-04'
+	},
+	{
+		value: 'A區-05'
+	},
+	{
+		value: 'A區-06'
+	},
+	{
+		value: 'A區-07'
+	},
+	{
+		value: 'A區-08'
+	},
+	{
+		value: 'A區-09'
+	},
+	{
+		value: 'A區-10'
+	},
+]
+
+export const Media_promotion_dropdown_menu: Select_item[] = [
+	{
+		value: '官網曝光'
+	},
+	{
+		value: 'EDM電子報'
+	},
+	{
+		value: '社群媒體'
+	},
+	{
+		value: '現場看板'
+	},
+	{
+		value: '會後新聞稿'
+	}, {
+		value: '攤位影音直擊'
+	}, {
+		value: '攤位開箱'
+	},
+	{
+		value: '專業人士專場導覽'
+	},
+]
+
+export const Add_to_cart_dropdown_menu: Select_item[] = [
+	{
+		value: '大會瓶裝水'
+	},
+	{
+		value: '獨家識別織帶'
+	},
+]
+
+export const Sponsorship_benefits_drop_down_menu: Select_item[] = [
+	{
+		value: 'Diamond (不知講堂/標準攤位)'
+	},
+	{
+		value: 'Gold (不知講堂/標準攤位)'
+	},
+	{
+		value: 'Silver (不知講堂/標準攤位)'
+	},
+	{
+		value: 'Bronze (標準攤位)'
+	},
+	{
+		value: 'General (標準攤位)'
+	},
+]
+
+export interface Select_item {
+	value: string;
+}
