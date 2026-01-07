16713302e995e27e6895cfaf6cea8a7e66052c4e
max liu <max.liu@digitimes.com>
2026-01-07 12:14:50 +0800

    新增:整合MainStore至展覽資訊預覽UI組件，新增權益檢查邏輯及優化內容顯示

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from "primeng/toast";
 import { MessageService } from "primeng/api";
+import { MainStore } from "./store/main.store";
 
 @Component({
 	selector: 'app-root',
-	imports: [ RouterOutlet, Toast ],
-	providers: [ MessageService ],
+	imports: [RouterOutlet, Toast],
+	providers: [MessageService, MainStore],
 	templateUrl: './app.html',
 	styleUrl: './app.css'
 })
--- src/app/exhibition-info-preview-ui/exhibition-info-preview-ui.ts
+++ src/app/exhibition-info-preview-ui/exhibition-info-preview-ui.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MainStore } from "../store/main.store";
+import { Select_item } from "../../mock-data/data";
 
 @Component({
 	selector: 'app-exhibition-info-preview-ui',
@@ -9,4 +10,19 @@ import { MainStore } from "../store/main.store";
 })
 export class ExhibitionInfoPreviewUi {
 	mainStore = inject(MainStore)
+	hasExhibitionRights = computed(() => {
+		const rights = this.mainStore.selected_exhibition_right()
+		return rights && Object.values(rights).some(value => {
+			if (Array.isArray(value)) {
+				return value.length > 0;
+			}
+			return value !== null && value !== undefined && value !== '';
+		});
+	})
+
+	// 判定 selected_exhibition_right物件裡的属性是否有值，用computed 屬性實現
+
+	renderArrayContent(items: Select_item[]) {
+		return items.map(i => i.value).join(', ')
+	}
 }
