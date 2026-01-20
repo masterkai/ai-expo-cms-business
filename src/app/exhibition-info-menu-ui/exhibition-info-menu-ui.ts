87c7cb38ee2c707b97ed7cf04059340b4af5b222
max liu <max.liu@digitimes.com>
2026-01-20 18:13:40 +0800

    新增攤位樣式選擇及格數輸入功能，優化展覽資訊頁面互動性

--- src/app/exhibition-info-menu-ui/exhibition-info-menu-ui.ts
+++ src/app/exhibition-info-menu-ui/exhibition-info-menu-ui.ts
@@ -1,17 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { MultiSelectModule } from 'primeng/multiselect';
 import { MainStore } from "../tables/exhibitors/store/main.store";
+import { Select } from "primeng/select";
+import { InputNumber } from "primeng/inputnumber";
 
 @Component({
 	selector: 'app-exhibition-info-menu-ui',
 	imports: [
 		FormsModule,
-		MultiSelectModule
+		MultiSelectModule,
+		Select,
+		InputNumber
 	],
 	templateUrl: './exhibition-info-menu-ui.html',
 	styleUrl: './exhibition-info-menu-ui.scss',
 })
 export class ExhibitionInfoMenuUi {
 	mainStore = inject(MainStore)
+	gridNum = signal(0);
+	// 攤位樣式(設計，標準，素地，新創)
+	boothStyles = [
+		{ name: '設計', code: 'design' },
+		{ name: '標準', code: 'standard' },
+		{ name: '素地', code: 'raw' },
+		{ name: '新創', code: 'startup' }
+	]
+	selectedBoothStyles = signal<string[]>([]);
+
+	onGridNumChange(value: number) {
+		this.gridNum.set(value);
+	}
+
+
 }
