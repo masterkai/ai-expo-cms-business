9f004631236b8df2a2ed3237b6515cc01f2ccf8f
max liu <max.liu@digitimes.com>
2026-01-22 14:04:07 +0800

    重構展覽資訊菜單組件，簡化狀態管理邏輯，移除不必要的訂閱與變更檢查

--- src/app/exhibition-info-menu-ui/exhibition-info-menu-ui.ts
+++ src/app/exhibition-info-menu-ui/exhibition-info-menu-ui.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { MultiSelectModule } from 'primeng/multiselect';
 import { MainStore } from "../tables/exhibitors/store/main.store";
 import { Select, SelectChangeEvent } from "primeng/select";
 import { InputNumber, InputNumberInputEvent } from "primeng/inputnumber";
 import { toObservable } from "@angular/core/rxjs-interop";
-import { Subscription } from "rxjs";
 
 @Component({
 	selector: 'app-exhibition-info-menu-ui',
@@ -30,40 +29,24 @@ export class ExhibitionInfoMenuUi {
 		{ name: '新創', code: 'startup' }
 	]
 	selectedBoothStyles: BoothStyles | null = null;
-	private cd = inject(ChangeDetectorRef);
-	private subs: Subscription[] = [];
 
-	ngOnInit() {
-		this.subs.push(
-			toObservable(this.mainStore.booth_style).subscribe({
-				next: (booth_style) => {
-					if (booth_style === '') {
-						this.selectedBoothStyles = null;
-					} else {
-						this.selectedBoothStyles = this.boothStyles.find(style => style.name === booth_style) || null;
-					}
-					// 確保在同一輪變更檢查後再觸發檢查，避免 ExpressionChangedAfterItHasBeenCheckedError
-					Promise.resolve().then(() => this.cd.detectChanges());
+	constructor() {
+		toObservable(this.mainStore.booth_style).subscribe({
+			next: (booth_style) => {
+				console.log('booth_style changed:', booth_style);
+				if (booth_style === '') {
+					this.selectedBoothStyles = null;
 				}
-			})
-		);
-
-		this.subs.push(
-			toObservable(this.mainStore.grid_num).subscribe({
-				next: (grid_num) => {
-					if (grid_num === '') {
-						this.gridNum = 1;
-					} else {
-						this.gridNum = parseInt(grid_num, 10);
-					}
-					Promise.resolve().then(() => this.cd.detectChanges());
+			}
+		})
+		toObservable(this.mainStore.grid_num).subscribe({
+			next: (grid_num) => {
+				console.log('grid_num changed:', grid_num);
+				if (grid_num === '') {
+					this.gridNum = 1;
 				}
-			})
-		);
-	}
-
-	ngOnDestroy() {
-		this.subs.forEach(s => s.unsubscribe());
+			}
+		})
 	}
 
 	onGridNumChange($event: InputNumberInputEvent) {
