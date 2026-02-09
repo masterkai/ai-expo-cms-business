a9aab03701ab8fd9ccee99d8f202cb5f9883882b
max liu <max.liu@digitimes.com>
2026-02-09 11:57:41 +0800

    在專屬連結欄位旁新增action

--- src/app/tables/exhibitors/exhibitors.ts
+++ src/app/tables/exhibitors/exhibitors.ts
@@ -1,130 +1,132 @@
 import { AfterViewInit, Component, inject, viewChild } from '@angular/core';
-import { TableModule } from "primeng/table";
-import { DatePipe } from "@angular/common";
-import { Button } from "primeng/button";
-import { MainStore } from "./store/main.store";
-import { MessageService } from "primeng/api";
-import { Tooltip } from "primeng/tooltip";
-import { Option } from "../../services/exhibition-rights-service";
-import { CommonDialog } from "../../shared/components/common-dialog/common-dialog";
-import {
-	ExhibitionRightsSettingProcess
-} from "../../exhibition-rights-setting-process/exhibition-rights-setting-process";
-import { Company } from "./store/main.slice";
-import { FileDownload } from "../../services/file-download";
-import { CompanyUpdateReviewService } from "../../services/company-update-review.service";
-import { toObservable } from "@angular/core/rxjs-interop";
+import { TableModule } from 'primeng/table';
+import { DatePipe } from '@angular/common';
+import { Button } from 'primeng/button';
+import { MainStore } from './store/main.store';
+import { MessageService } from 'primeng/api';
+import { Tooltip } from 'primeng/tooltip';
+import { Option } from '../../services/exhibition-rights-service';
+import { CommonDialog } from '../../shared/components/common-dialog/common-dialog';
+import { ExhibitionRightsSettingProcess } from '../../exhibition-rights-setting-process/exhibition-rights-setting-process';
+import { Company } from './store/main.slice';
+import { FileDownload } from '../../services/file-download';
+import { CompanyUpdateReviewService } from '../../services/company-update-review.service';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
-	selector: 'app-exhibitors',
-	imports: [
-		TableModule,
-		DatePipe,
-		Button,
-		Tooltip,
-		CommonDialog,
-		ExhibitionRightsSettingProcess,
-	],
-	templateUrl: './exhibitors.html',
-	styleUrl: './exhibitors.css',
-	standalone: true
+  selector: 'app-exhibitors',
+  imports: [TableModule, DatePipe, Button, Tooltip, CommonDialog, ExhibitionRightsSettingProcess],
+  templateUrl: './exhibitors.html',
+  styleUrl: './exhibitors.css',
+  standalone: true,
 })
 export class Exhibitors implements AfterViewInit {
-	rightSettingProcess = viewChild(ExhibitionRightsSettingProcess)
-	messageService = inject(MessageService)
-	modifyProcessService = inject(CompanyUpdateReviewService)
-	mainStore = inject(MainStore);
-	dialog = viewChild(CommonDialog)
-	downloader = inject(FileDownload)
+  rightSettingProcess = viewChild(ExhibitionRightsSettingProcess);
+  messageService = inject(MessageService);
+  modifyProcessService = inject(CompanyUpdateReviewService);
+  mainStore = inject(MainStore);
+  dialog = viewChild(CommonDialog);
+  downloader = inject(FileDownload);
 
-	constructor() {
-		toObservable(this.mainStore.current_compID).subscribe({
-			next: (compID) => {
-				console.log('current_compID changed:', compID);
-				if (compID !== '' && compID !== null) {
-					this.mainStore.getBoothSpec(compID)
-				}
-			}
-		})
-	}
+  constructor() {
+    toObservable(this.mainStore.current_compID).subscribe({
+      next: (compID) => {
+        console.log('current_compID changed:', compID);
+        if (compID !== '' && compID !== null) {
+          this.mainStore.getBoothSpec(compID);
+        }
+      },
+    });
+  }
 
-	ngAfterViewInit() {
-		this.dialog()?.visible$.subscribe({
-			next: (value) => {
-				console.log('Dialog visible changed:', value);
-				// if dialog is closed, clear current company selection
-				// and reset selected exhibition rights
-				// and current company ID in the store
-				if (!value) {
-					this.rightSettingProcess()!.stepper.value.set(1)
-					this.mainStore.setCurrentCompany(null)
-					this.mainStore.resetSelectedExhibitionRights()
-					this.mainStore.setCurrentCompID(null)
-					this.mainStore.setSelectedSponsorShips(null)
-					this.mainStore.setBoothStyle('')
-					this.mainStore.setGridNumber('')
-				}
-				this.mainStore.setIsDialogVisible(value);
-			}
-		})
-	}
+  ngAfterViewInit() {
+    this.dialog()?.visible$.subscribe({
+      next: (value) => {
+        console.log('Dialog visible changed:', value);
+        // if dialog is closed, clear current company selection
+        // and reset selected exhibition rights
+        // and current company ID in the store
+        if (!value) {
+          this.rightSettingProcess()!.stepper.value.set(1);
+          this.mainStore.setCurrentCompany(null);
+          this.mainStore.resetSelectedExhibitionRights();
+          this.mainStore.setCurrentCompID(null);
+          this.mainStore.setSelectedSponsorShips(null);
+          this.mainStore.setBoothStyle('');
+          this.mainStore.setGridNumber('');
+        }
+        this.mainStore.setIsDialogVisible(value);
+      },
+    });
+  }
 
-	copyLinkToClipboard(exhibitor: Company) {
-		const url = exhibitor.link
-		console.log('copyLinkToClipboard', url);
-		navigator.clipboard.writeText(url)
-			.then(() => {
-				console.log("連結已複製到剪貼簿");
-				this.messageService.add({ severity: 'success', summary: '成功', detail: '連結已複製到剪貼簿' });
-			}).then(() => {
-				this.mainStore.onGetLink(exhibitor.unified_business_no)
-			})
-			.catch(err => {
-				console.error("複製失敗：", err);
-				this.messageService.add({ severity: 'error', summary: '失敗', detail: '連結複製失敗' });
-			});
-	}
+  copyLinkToClipboard(exhibitor: Company) {
+    const url = exhibitor.link;
+    console.log('copyLinkToClipboard', url);
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        console.log('連結已複製到剪貼簿');
+        this.messageService.add({
+          severity: 'success',
+          summary: '成功',
+          detail: '連結已複製到剪貼簿',
+        });
+      })
+      .then(() => {
+        this.mainStore.onGetLink(exhibitor.unified_business_no);
+      })
+      .catch((err) => {
+        console.error('複製失敗：', err);
+        this.messageService.add({ severity: 'error', summary: '失敗', detail: '連結複製失敗' });
+      });
+  }
 
-	renderArrayContent(items: Option[]) {
-		return items.map(i => i.option).join(', ')
-	}
+  renderArrayContent(items: Option[]) {
+    return items.map((i) => i.option).join(', ');
+  }
 
-	protected exportCompanyCSV() {
-		this.modifyProcessService.getHistoryDownload({
-			cate: 'company',
-			empno: this.mainStore.empno() || '',
-			departid: this.mainStore.departid() || ''
-		}).subscribe({
-			next: (response) => {
-				if (response.status === 'success' && response.data?.download_url) {
-					const url = response.data.download_url;
-					const filename = '參展商列表.csv';
-					this.downloader.downloadCsv(url, filename).subscribe({
-						next: () => {
-							this.messageService.add({
-								severity: 'success',
-								summary: '下載完成',
-								detail: '公司資料下載完成'
-							});
-						},
-						error: (err) => {
-							this.messageService.add({
-								severity: 'error',
-								summary: '下載失敗',
-								detail: `公司資料下載失敗。${err.message ? ' ' + err.message : ''}`
-							});
-						}
-					});
-				}
-			}
-		})
+  protected exportCompanyCSV() {
+    this.modifyProcessService
+      .getHistoryDownload({
+        cate: 'company',
+        empno: this.mainStore.empno() || '',
+        departid: this.mainStore.departid() || '',
+      })
+      .subscribe({
+        next: (response) => {
+          if (response.status === 'success' && response.data?.download_url) {
+            const url = response.data.download_url;
+            const filename = '參展商列表.csv';
+            this.downloader.downloadCsv(url, filename).subscribe({
+              next: () => {
+                this.messageService.add({
+                  severity: 'success',
+                  summary: '下載完成',
+                  detail: '公司資料下載完成',
+                });
+              },
+              error: (err) => {
+                this.messageService.add({
+                  severity: 'error',
+                  summary: '下載失敗',
+                  detail: `公司資料下載失敗。${err.message ? ' ' + err.message : ''}`,
+                });
+              },
+            });
+          }
+        },
+      });
+  }
 
+  protected onClickExhibitorItem(exhibitor: Company) {
+    this.mainStore.getExhibitionRights({ id: exhibitor.unified_business_no, type: '' });
+    this.mainStore.setCurrentCompany(exhibitor);
+    this.mainStore.setIsDialogVisible(true);
+  }
 
-	}
-
-	protected onClickExhibitorItem(exhibitor: Company) {
-		this.mainStore.getExhibitionRights({ id: exhibitor.unified_business_no, type: '' });
-		this.mainStore.setCurrentCompany(exhibitor)
-		this.mainStore.setIsDialogVisible(true);
-	}
+  protected onReviewItem(exhibitor: Company) {
+    const uninum = exhibitor.unified_business_no;
+    console.log('unino', uninum);
+  }
 }
